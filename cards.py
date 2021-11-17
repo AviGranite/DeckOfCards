987c61df820e86ae55cb9a3162951a3e91e583c5
Avi Granite <73539664+AviGranite@users.noreply.github.com>
2021-11-17 17:22:18 -0500

    Add files via upload

---
+++ cards.py
@@ -0,0 +1,21 @@
+import random
+import itertools
+
+suits = ('H', 'D', 'S', 'C')
+digits = [str(i) for i in range(2, 11)]
+face = ['Jack', 'Queen', 'King']
+values = digits + face + ['Ace']
+deck = []
+
+for i in suits:
+    deck.extend(list(zip(values, itertools.repeat(i))))
+
+random.shuffle(deck) #shuffle
+#print(deck)
+deck_g = (c for c in deck) #deck as a generator
+
+
+
+
+
+
